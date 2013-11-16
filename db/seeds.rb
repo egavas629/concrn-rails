b34f8db707fd77861da71e248bbce60a3c0cb980
GavMo <quavmo@gmail.com>
2013-11-16 14:10:31 -0800

    Add dan to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,20 @@
-Report.destroy_all
+def create_reports
+    Report.destroy_all
 
-Report.create({
-    name: "Ella",
-    phone: "209-559-2459",
-    lat: 37.782983333333334,
-    long: 122.4065,
-    nature: "they tuk our jobs"
-    })
-
-15.times do
-  Report.create({
-    name: Faker::Name.name,
-    phone: Faker::PhoneNumber.cell_phone,
-    lat: rand(200),
-    long: rand(200),
-    nature: Faker::Company.bs
-    })
+    15.times do
+      Report.create({
+        name: Faker::Name.name,
+        phone: Faker::PhoneNumber.cell_phone,
+        lat: rand(200),
+        long: rand(200),
+        nature: Faker::Company.bs
+        })
+    end
 end
+
+def create_accounts
+    User.create!(email: 'dan@example.com', role: 'dispatcher', password: 'password')
+end
+
+create_reports
+create_accounts
