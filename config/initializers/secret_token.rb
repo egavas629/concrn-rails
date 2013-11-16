16c77605da43adafc8c1195705de7947d038a3e1
GavMo <quavmo@gmail.com>
2013-11-16 10:17:17 -0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Streetmom::Application.config.secret_key_base = '5abef5ec5ac032954e1fe6ac8ef53298a75f194cf121d79f52175024c060e2e875ca1177179a6d5bbf6089f77a6296fb1e630a40af7c83b739949b4c86b723a7'
