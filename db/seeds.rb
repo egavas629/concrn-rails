cf9b55c28329f7f76822fb0c99a28606fe63216c
GavMo <quavmo@gmail.com>
2013-11-17 10:04:34 -0800

    Tommy is Responder

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,8 +23,10 @@ def create_accounts
     User.destroy_all
     Dispatcher.create!(name: 'Dan', email: 'dan@example.com', password: 'password')
     Dispatcher.create!(name: 'Doug', email: 'dbmarks@gmail.com', password: 'password')
+
     Responder.create!(name: 'Jacob', email: 'jacobcsavage@gmail.com', password: 'password', phone: '6502481396')
     Responder.create!(name: 'Gavin', email: 'quavmo@gmail.com', password: 'password', phone: '6507876770')
+    Responder.create!(name: 'Tommy', email: 'vajrapani666@gmail.com', password: 'password', phone: '(209) 559-2459')
 end
 
 create_reports
