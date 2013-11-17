4fc9a71de893ec319ba15e4f7b131964de4d037a
Thomas Devol <pair+thomas@rally.org>
2013-11-16 19:59:14 -0800

    Auto reload dispatch screen if rejection occurs

--- app/jobs/expire_pending_dispatches.rb
+++ app/jobs/expire_pending_dispatches.rb
@@ -4,6 +4,10 @@ class ExpirePendingDispatches
   end
 
   def self.perform
-    Dispatch.where(status: "pending").where("created_at < ?", 2.minutes.ago).update_all(status: "rejected", rejection_reason: "timeout")
+    stales = Dispatch.where(status: "pending").where("created_at < ?", 2.minutes.ago)
+    stales.each do |dispatch|
+      dispatch.update_attributes(status: "rejected", rejection_reason: "timeout")
+    end
+    Pusher.trigger("reports" , "report_created", {}) if stales.any?
   end
 end
