1f889fef6252938ff56da384e8b856cf95ba02d7
GavMo <quavmo@gmail.com>
2013-11-16 20:21:13 -0800

    Placeholder for responders list && minimal seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ def create_reports
     p 'Seeding Reports...'
     Report.destroy_all
 
-    15.times do
+    2.times do
       address = Faker::Address
       Report.create({
         name: Faker::Name.name,
@@ -11,7 +11,7 @@ def create_reports
         long: 122.416667 + (rand() * (rand() > 0.5 ? -1 : 1)),
         address: address.street_address,
         age: "Young Adult",
-        gender: "Male",
+        gender: ["Male", "Female"].sample,
         race: "Caucasian",
         nature: Faker::Company.bs
         })
@@ -25,21 +25,6 @@ def create_accounts
     Dispatcher.create!(name: 'Doug', email: 'dbmarks@gmail.com', password: 'password')
     Responder.create!(name: 'Jacob', email: 'jacobcsavage@gmail.com', password: 'password', phone: '6502481396')
     Responder.create!(name: 'Gavin', email: 'quavmo@gmail.com', password: 'password', phone: '6507876770')
-
-    create_responders
-end
-
-def create_responders
-    p 'Seeding Responders...'
-    15.times do
-      Responder.create!(
-        name: Faker::Name.name,
-        phone: Faker::PhoneNumber.cell_phone,
-        email: Faker::Internet.email,
-        password: "password",
-        password_confirmation: "password"
-      )
-    end
 end
 
 create_reports
