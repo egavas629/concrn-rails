3876064548444ce7cd38925065a548acdd39c46b
Thomas Devol <pair+thomas@rally.org>
2013-11-24 20:52:53 -0800

    Unassigned uses pending for status

--- app/models/report.rb
+++ app/models/report.rb
@@ -7,7 +7,7 @@ class Report < ActiveRecord::Base
       SELECT r.*, count(distinct d.id) as ad_count, count(distinct dr.id) as dr_count FROM reports r
         LEFT JOIN dispatches d on d.report_id=r.id
         LEFT JOIN dispatches dr on dr.report_id=r.id AND dr.status='rejected'
-      WHERE r.status = 'unassigned'
+      WHERE r.status = 'pending'
       GROUP BY r.id
       HAVING count(distinct d.id) = count(distinct dr.id)
       })
