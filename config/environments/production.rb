24b4d84c8fa65dfb13093d75e414d5210b66e473
GavMo <quavmo@gmail.com>
2013-12-17 21:15:46 -0800

    No stinking .PSDs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Streetmom::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
   config.serve_static_assets = true
   config.assets.compile = true
@@ -13,5 +13,5 @@ Streetmom::Application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
-  config.time_zone =
+  config.time_zone = "Pacific Time (US & Canada)"
 end
