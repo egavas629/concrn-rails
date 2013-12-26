fe93dafb780116989d0a14c0961cc6b8c380ff62
GavMo <quavmo@gmail.com>
2013-12-25 22:43:04 -0800

    Marks forwarded logs [Finishes #62972748]

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,8 +1,14 @@
 class Log < ActiveRecord::Base
+  default_scope -> { order :created_at }
+
   belongs_to :report
   belongs_to :author, class_name: 'User'
 
   def broadcast
-    Message.send body, to: report.responder.phone
+    touch(:updated_at) if Message.send(body, to: report.responder.phone)
+  end
+
+  def broadcasted?
+    created_at != updated_at
   end
 end
