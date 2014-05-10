38263182b6028b12ce2a81134b010d704cd769bf
Gavin Morgan <quavmo@gmail.com>
2014-05-10 14:42:28 -0700

    Set productin logger to STDOUT

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ Streetmom::Application.configure do
   config.assets.js_compressor = :uglifier
   config.assets.version = '1.0'
   config.log_level = :info
+  config.logger = Logger.new(STDOUT)
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
