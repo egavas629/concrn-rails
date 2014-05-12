be641edfd4fd34b3cb41a72f1f38aed07f742e71
Thomas Devol <pair+thomas@rally.org>
2014-05-12 15:56:57 -0700

    Add jumbo cafe image, code organization and make css a comfortable land of mixins [#71186070]

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
 module PagesHelper
+  def conditional_nav_tag(&block)
+    classes = %w(navbar navbar-fixed-top)
+    classes << "home" if params[:controller] == "pages" && params[:action] == "home"
+    content_tag :div, {class: classes, role: "navigation"}, &block
+  end
 end
