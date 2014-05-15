286adfd1428420fa65eb6331a116ed04ac8c7979
Thomas Devol <pair+thomas@rally.org>
2014-05-15 09:52:12 -0700

    Allow setting of SMS phone origination #

--- app/models/telephony.rb
+++ app/models/telephony.rb
@@ -1,5 +1,5 @@
 class Telephony
-  OUTGOING_PHONE = '(978) 566-1976'
+  OUTGOING_PHONE = ENV['SMS_PHONE'] || '(978) 566-1976'
 
   def self.message to: nil, body: 'Thanks!'
     client.account.messages.create(from: OUTGOING_PHONE, to: to, body: body)
