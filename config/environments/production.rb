410bc85ad425a57cee282602dd02cd1f743d5cc3
Barnett Klane <blklane@syr.edu>
2014-06-14 13:49:42 -0700

    fix environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,18 @@
 Streetmom::Application.configure do
-  require Rails.root.join("config/environments/production")
+  # Settings specified here will take precedence over those in config/application.rb.
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
+  config.assets.js_compressor = :uglifier
+  config.assets.version = '1.0'
+  config.log_level = :debug
+  config.logger = Logger.new(STDOUT)
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+  config.log_formatter = ::Logger::Formatter.new
+  config.time_zone = "Pacific Time (US & Canada)"
 end
