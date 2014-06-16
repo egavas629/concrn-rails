eec26eeaf779a4a33a1edf01c8319e52177e826b
Thiggis <doug@concrn.com>
2014-06-16 10:57:56 -0700

    Fixed seeds, commented out user.rb availability

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
     phone         { '2133733979' }
     password      { 'password' }
     name          { Faker::Name.name }
-    availability  { false }
+    # availability  { false }
   end
 
   factory :responder do
@@ -12,7 +12,7 @@ FactoryGirl.define do
     phone         { ['5103874543', '2133733979'].sample }
     password      { 'password' }
     name          { Faker::Name.name }
-    availability  { true }
+    # availability  { true }
 
     trait(:jacob) do
      name "Jacob"
