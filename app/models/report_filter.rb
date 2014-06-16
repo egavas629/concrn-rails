39407c275fcd2c18507721a7fdde57dc0b69a1b4
Barnett Klane <blklane@syr.edu>
2014-06-15 21:25:14 -0700

    history date filter [Finishes #73167156]

--- app/models/report_filter.rb
+++ app/models/report_filter.rb
@@ -8,17 +8,17 @@ class ReportFilter
     order       = order.present? ? order : 'desc' && params[:order] = 'desc'
     @order      = "created_at #{order}"
 
-    @start_date = Date.parse(start_date).end_of_day if start_date.present?
+    @start_date = Date.parse(start_date).beginning_of_day if start_date.present?
     @end_date   = Date.parse(end_date).end_of_day if end_date.present?
   end
 
   def query
     if defined?(@start_date) && defined?(@end_date)
-      reports = completed_reports.where('Date(created_at) >= ? and Date(created_at) <= ?', @start_date, @end_date)
+      reports = completed_reports.where('created_at >= ? and created_at <= ?', @start_date, @end_date)
     elsif defined?(@start_date)
-      reports = completed_reports.where('Date(created_at) >= ?', @start_date)
+      reports = completed_reports.where('created_at >= ?', @start_date)
     elsif defined?(@end_date)
-      reports = completed_reports.where('Date(created_at) <= ?', @end_date)
+      reports = completed_reports.where('created_at <= ?', @end_date)
     else
       reports = completed_reports.where(@params)
     end
