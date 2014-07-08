383d98f0dfc1730402efd72fec471843a6e4b5df
Barnett Klane <blklane@syr.edu>
2014-07-08 11:07:59 -0700

    uses pusher url if defined

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,4 @@
 require 'pusher'
-Pusher.url = "http://#{ENV['PUSHER_KEY']}:#{ENV['PUSHER_SECRET']}@api.pusherapp.com/apps/#{ENV['PUSHER_APP_ID']}"
+
+Pusher.url = ENV['PUSHER_URL'] || "http://#{ENV['PUSHER_KEY']}:#{ENV['PUSHER_SECRET']}@api.pusherapp.com/apps/#{ENV['PUSHER_APP_ID']}"
 Pusher.logger = Rails.logger
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,27 +1,33 @@
 require 'faker'
 
 FactoryGirl.define do
-  factory :dispatcher do
-    email         { "#{name.downcase.gsub(/[ .]/, '.' => '', ' ' => '.')}@gmail.com" }
+  factory :user do
+    email         { "#{name_to_email(name)}@gmail.com" }
     phone         { Faker::PhoneNumber.phone_number }
     password      { 'password' }
     name          { Faker::Name.name }
-  end
 
-  factory :responder do
-    email         { "#{name.downcase.gsub(' ','.')}@gmail.com" }
-    phone         { Faker::PhoneNumber.phone_number }
-    password      { 'password' }
-    name          { Faker::Name.name }
-    active        { true }
+    factory :dispatcher, class: Dispatcher do
+      role { 'dispatcher' }
+    end
+
+    factory :responder, class: Responder do
+      role   {'responder'}
+      active { true }
+
+      trait(:on_shift) do
+        after(:create) { |r| create(:shift, responder: r) }
+      end
+    end
 
     trait(:jacob) do
-     name "Jacob"
-     phone '6502481396'
-    end
-
-    trait(:on_shift) do
-      after(:create) { |r| create(:shift, responder: r) }
+      name  { 'Jacob' }
+      phone { '6502481396' }
+      email { 'jacob@concrn.com' }
     end
   end
+end
+
+def name_to_email(name)
+  name.downcase.gsub(/[ .]/, '.' => '', ' ' => '.')
 end
