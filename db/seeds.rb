60c32ada25bfa6a3543d5e475f9599fa0c6e3813
Barnett Klane <blklane@syr.edu>
2014-07-16 12:44:12 -0700

    cleanup seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,21 @@
-def reports
+def reports(agency_id)
     puts 'Seeding Reports...'
     Report.destroy_all
-    3.times { FactoryGirl.create :report }
+    3.times { FactoryGirl.create :report, agency_id: agency_id }
 end
 
-def accounts
+def accounts(agency_id)
     puts 'Seeding Teammates...'
     User.destroy_all
 
-    dispatcher = FactoryGirl.create :dispatcher, name: 'Dispatcher Dan', email: 'dan@concrn.com', password: 'password'
-    7.times { FactoryGirl.create :responder }
+    FactoryGirl.create :dispatcher, name: 'Dispatcher Dan',
+      email: 'dan@concrn.com', password: 'password', agency_id: agency_id
+
+    7.times { FactoryGirl.create :responder,  agency_id: agency_id }
 end
 
 
-reports
-accounts
+agency_id = FactoryGirl.create(:agency).id
+
+reports(agency_id)
+accounts(agency_id)
