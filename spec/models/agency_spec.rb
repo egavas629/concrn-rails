ef3e0447c28035d042a924d591db537641d966b1
Thiggis <doug@concrn.com>
2014-07-24 23:03:31 -0700

    Am I learning or unlearning?

--- spec/models/agency_spec.rb
+++ spec/models/agency_spec.rb
@@ -9,7 +9,6 @@ describe Agency do
   it { should validate_presence_of(:address) }
   it { should validate_presence_of(:text_phone) }
   it { should validate_presence_of(:call_phone) }
-  it { should validate_presence_of(:zip_code_list) }
   it { should validate_uniqueness_of(:name) }
   it { should validate_uniqueness_of(:address) }
   it { should validate_uniqueness_of(:text_phone) }
