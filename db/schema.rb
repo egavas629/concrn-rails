76068b182a039319aa1bed5f1b1d1bb11fd297f7
Thiggis <doug@concrn.com>
2014-07-29 14:23:06 -0700

    Schema fix for zip_code_list

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140725061443) do
+ActiveRecord::Schema.define(version: 20140729212016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20140725061443) do
     t.text     "address"
     t.string   "call_phone"
     t.string   "text_phone"
-    t.text     "zip_code_list"
   end
 
   create_table "dispatches", force: true do |t|
