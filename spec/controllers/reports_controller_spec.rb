21227da993a644a44565a28ab3fc8df935092f65
Jesse Szwedko <jesse.szwedko@gmail.com>
2016-04-02 16:32:11 -0700

    Only notify responders once when a report is complete
    We were sending duplicate complete messages because:
    
    * We were marking the report as complete multiple times (which sent
      caused the hook to fire multiple times)
    * For each notification, we were notifying every responder

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -25,4 +25,32 @@ describe ReportsController do
   #     end
   #   end
   # end
+
+  context "when completing report" do
+    let(:report) { create(:report, :accepted) }
+    let(:additional_responder) { create(:user, :responder) }
+
+    before do
+      request.env['HTTP_REFERER'] = '/reports'
+    end
+
+    it "notifies dispatchers the report is complete" do
+      report.dispatch(additional_responder.responder)
+
+      report.dispatches.each do |dispatch|
+        dispatch.update_attribute(:status, :accepted)
+      end
+
+      notified = []
+      allow(Telephony).to receive(:send) do |body, to|
+        next unless body =~ /The report is now completed/
+        fail "#{to} has already been notified" if notified.include?(to)
+        notified << to
+      end
+
+      put :update, id: report.id, report: { status: :completed }
+
+      notified.should have(report.responders.count).items
+    end
+  end
 end
