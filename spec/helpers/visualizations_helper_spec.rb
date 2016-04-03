65f120fcc78a08db374fb8e4dcab99f3cdd8e128
kimallen <kimmarieallen@gmail.com>
2016-04-02 18:08:10 -0700

    Start adding route for map page

---
+++ spec/helpers/visualizations_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the VisualizationsHelper. For example:
+#
+# describe VisualizationsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+describe VisualizationsHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
