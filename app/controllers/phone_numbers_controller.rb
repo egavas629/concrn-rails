c6c74999824e6115b2b142891d6738e528b882ea
Fatma Ocal <contactfatma@gmail.com>
2016-04-02 21:50:08 -0700

    Work in progress for sms verification (Authentication is not yet working due to invalid username

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -4,11 +4,19 @@ class PhoneNumbersController < ApplicationController
   end
 
   def create
-    @phone_number = PhoneNumber.find_or_create_by(phone_number: params[:phone_number][:phone_number])
-    @phone_number.generate_pin
-    @phone_number.send_pin
-    respond_to do |format|
-      format.js # render app/views/phone_numbers/create.js.erb
-    end
+    @phone_number = PhoneNumber.find_or_create_by(phone_number:params[:phone_number][:phone_number])
+    @phone_number.generate_pin
+    @phone_number.send_pin
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def verify
+    @phone_number = PhoneNumber.find_by(phone_number: params[:hidden_phone_number])
+    @phone_number.verify(params[:pin])
+    respond_to do |format|
+      format.js
+    end
   end
 end
