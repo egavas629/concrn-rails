de45015d2f5190978e36f667cb6c7aeeeda8ce6a
kimallen <kimmarieallen@gmail.com>
2016-04-03 15:58:13 -0700

    Parse data to get reports by month chart

--- app/controllers/visualizations_controller.rb
+++ app/controllers/visualizations_controller.rb
@@ -5,10 +5,21 @@ class VisualizationsController < ApplicationController
 	end
 
 	def reports_charts
-		data = []
-		@ordered_reports = report.order(:created_at)
+		@reports = Report.all
 		@reports_by_month = []
-		@reports_by_month.each do |report|
-			report.created_at
+		@months_hash = {}
+		(1..12).each do |month|
+			@months_hash[month] = []
+		end
+
+		@reports_by_month = @reports.group_by {|report| report.created_at.month}
+		@data = []
+		@reports_by_month.each do |key, value|
+			@months_hash[key] = value
+		end
+		@months_hash.each do |key, value|
+			@data << value.count
+		end
+		@data
 	end
 end
