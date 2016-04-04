d17d5f406731609832b2f36cd9698c248376064f
Jesse Szwedko <jesse.szwedko@gmail.com>
2016-04-03 18:05:18 -0700

    Bring schema.rb up-to-date

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403191600) do
+ActiveRecord::Schema.define(version: 20160403223633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 20160403191600) do
     t.string   "phone_number"
     t.string   "pin"
     t.boolean  "verified"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reporter_locations", force: true do |t|
+    t.integer  "user_id"
+    t.float    "latitude",   null: false
+    t.float    "longitude",  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
