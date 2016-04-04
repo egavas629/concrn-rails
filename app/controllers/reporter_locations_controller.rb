0dad9f9ebbb677308226d6baeb27297d1d3f08da
supermikol <mdu123@gmail.com>
2016-04-03 17:05:09 -0700

    complete backend api for storing responder locations

--- app/controllers/reporter_locations_controller.rb
+++ app/controllers/reporter_locations_controller.rb
@@ -3,11 +3,13 @@ class ReporterLocationsController < ApplicationController
 def create
   @reporterlocation = ReporterLocation.new(reporterlocation_params)
   @reporterlocation.save
-  render json: {params}
+  puts params, "*****"
+  render json: params
 end
 
 private
   def reporterlocation_params
-    params.permit(:lat, :long, :user_id)
+    params.permit(:latitude, :longitude, :user_id)
   end
+
 end
