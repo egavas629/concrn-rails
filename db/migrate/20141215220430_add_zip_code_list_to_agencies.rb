7ef0e1e2f6bb538373574b021d16657f58bf4f66
Gavin Morgan <quavmo@gmail.com>
2016-04-13 19:02:24 -0700

    nerf troublesome migration

--- db/migrate/20141215220430_add_zip_code_list_to_agencies.rb
+++ db/migrate/20141215220430_add_zip_code_list_to_agencies.rb
@@ -1,5 +1,5 @@
 class AddZipCodeListToAgencies < ActiveRecord::Migration
   def change
-    add_column :agencies, :zip_code_list, :string
+    # add_column :agencies, :zip_code_list, :string
   end
 end
