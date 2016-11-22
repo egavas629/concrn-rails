7572efbcb940212611a49dc71611ebdb9a8bb1d2
Matt Harris <matrharr@gmail.com>
2016-11-21 18:41:50 -0800

    update spec syntax to account for version update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,9 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
   config.include Paperclip::Shoulda::Matchers
 
+  config.include(Shoulda::Matchers::ActiveModel, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+
   config.infer_base_class_for_anonymous_controllers = false
 
   config.after(:suite) do
