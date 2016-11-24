1ddc3151371a88a8debe4259f54aa087673cedc2
Matt Harris <matrharr@gmail.com>
2016-11-23 20:07:01 -0800

    configure shoulda matchers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,12 +5,20 @@ require 'capybara/rspec'
 require 'shoulda/matchers'
 require "paperclip/matchers"
 
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+
+    with.library :rails
+  end
+end
+
+
+
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
   config.include Paperclip::Shoulda::Matchers
-
-  config.include(Shoulda::Matchers::ActiveModel, type: :model)
-  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 
   config.infer_base_class_for_anonymous_controllers = false
 
