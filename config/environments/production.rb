222177e2527c002adb685f2b7b8113215f6e531b
Matt Harris <matrharr@gmail.com>
2016-12-09 14:25:46 -0800

    remove s3 region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,6 @@ Streetmom::Application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_region => ENV['AWS_REGION'],
     :s3_credentials => {
       :bucket => ENV["AWS_BUCKET"],
       :access_key_id => ENV["AWS_KEY"],
